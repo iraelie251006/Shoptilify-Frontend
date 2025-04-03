f19cf4db04adca02c7ae7699b351ee84c483e60e
Elie <niyubwayoiraelie5777@gmail.com>
2025-04-03 23:53:34 +0200

    Updated Tailwindcss Configuration, Added lucide-react dependency & initiated Shadcn UI

--- app/page.js
+++ app/page.js
@@ -1,27 +1,37 @@
 "use client"
 import api from '@/api/api';
-import CardContainer from '@/components/CardContainer';
-import Header from '@/components/Header';
+import CardContainer from '@/components/CardContainer'
+import Header from '@/components/Header'
 import { useEffect, useState } from 'react';
-const Home = () => {
 
-  const [products, setProducts] = useState([])
+const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [skeleton, setSkeleton] = useState(false);
 
   useEffect(() => {
-  api.get("products").then(res => {
-  console.log(res.data)
-  setProducts(res.data)
-}).catch((error) => {
-  console.log(error)
-})
-}, [])
+    setLoading(true);
+    setSkeleton(true); // Move this line here
+    api.get("products")
+      .then(res => {
+        console.log(res.data);
+        setProducts(res.data);
+        setLoading(false);
+        setSkeleton(false); // Set skeleton to false after loading
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+        setSkeleton(false); // Set skeleton to false on error
+      });
+  }, []);
 
   return (
     <>
       <Header />
-      <CardContainer products={products}/>
+      <CardContainer products={products} show={skeleton} />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- components/Card.jsx
+++ components/Card.jsx
@@ -1,15 +1,27 @@
 import { BASE_URL } from "@/api/api"
 import Image from "next/image"
+import { SkeletonCard } from "./Skeleton"
 
-const Card = ({product}) => {
+const Card = ({product, show}) => {
+    console.log(`${show}`)
+    const placeNumbers = [...Array(12).keys()].slice(0);
   return (
-    <div className="max-w-sm rounded-lg shadow-2xl w-[300px] ">
+    <>
+    {show ? (
+        <>
+        {placeNumbers.map(num => <SkeletonCard key={num}/>)}
+        </>
+    ) : (
+     <div className="max-w-sm rounded-lg shadow-2xl w-[300px] ">
         <Image src={`${BASE_URL}${product.image}`} className=" rounded-t-lg" alt="Card image" width={300} height={300}/>
         <div className="p-4">
             <h5 className="text-xl font-semibold">{`${product.name}`}</h5>
             <p className="text-gray-700">{`$${product.price}`}</p>
         </div>
-    </div>
+    </div>   
+    )}
+    
+    </>
   )
 }
 
